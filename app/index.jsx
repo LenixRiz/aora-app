c5208596df718308d7692446ae5513b5a2447c79
LenixRiz <rizky.fajarbusiness945@gmail.com>
2024-09-28 22:35:27 +0700

    add bottom bar

--- app/index.jsx
+++ app/index.jsx
@@ -5,9 +5,12 @@ import { Link } from 'expo-router';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="font-bold text-2xl">Hi Aora!</Text>
+      <Text className="text-3xl font-pblack">Hi Aora!</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" style={{ color: 'blue'}}>Go to Profile</Link>
+      <Link href="/home" style={{ color: 'blue'}}>Go to home</Link>
+      <Link href="/bookmark" style={{ color: 'blue'}}>Go to bookmark</Link>
+      <Link href="/create" style={{ color: 'blue'}}>Go to create</Link> 
+      <Link href="/profile" style={{ color: 'blue'}}>Go to profile</Link>
     </View>
   );
 }
